a8ca431945bb7004bb196462b1f3b886e3af1dc5
Sandra Dopico Cantarero <sandra.dopico1@udc.es>
2018-10-16 12:34:07 +0200

    Eliminacion de includes

--- ASIP1/rest-example-master/src/main/java/es/udc/lbd/asi/restexample/model/repository/MovieDAO.java
+++ ASIP1/rest-example-master/src/main/java/es/udc/lbd/asi/restexample/model/repository/MovieDAO.java
@@ -3,11 +3,10 @@ package es.udc.lbd.asi.restexample.model.repository;
 import java.util.List;
 
 import es.udc.lbd.asi.restexample.model.domain.Movie;
-import es.udc.lbd.asi.restexample.model.domain.Post;
+
 
 public interface MovieDAO {
-    List<Movie> findAll();
-
-    Movie findById(Long id);
+    public List<Movie> findAll();
+    public Movie findById(Long id);
 
 }
