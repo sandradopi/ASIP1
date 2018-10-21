cce4fc42e17106f02768ed9be175269912bb48af
Sandra Dopico Cantarero <sandra.dopico1@udc.es>
2018-10-21 14:15:21 +0200

    Un cambio en clase GlobalException que visualice en los commits de Alejandro

--- ASIP1/rest-example-master/src/main/java/es/udc/lbd/asi/restexample/web/util/GlobalControllerExceptionHandler.java
+++ ASIP1/rest-example-master/src/main/java/es/udc/lbd/asi/restexample/web/util/GlobalControllerExceptionHandler.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import es.udc.lbd.asi.restexample.web.exception.IdAndBodyNotMatchingOnUpdateException;
+import es.udc.lbd.asi.restexample.web.exception.ResourceException;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(IdAndBodyNotMatchingOnUpdateException.class)
-    public ErrorDTO idAndBodyNotMatchingOnUpdateExceptionHandler(IdAndBodyNotMatchingOnUpdateException e) {
-        return new ErrorDTO(e.getMessage());
-    }
+	 @ResponseStatus(HttpStatus.NOT_FOUND)
+	    @ExceptionHandler(ResourceException.class)
+	    public ErrorDTO resourceExceptionHandler(ResourceException e) {
+	        return new ErrorDTO(e.getMessage());
+	    }
 }
