0796c69ed38f2479e9517b9bb932437113f0ec7a
Sandra Dopico Cantarero <sandra.dopico1@udc.es>
2018-11-01 16:04:58 +0100

    botones delete y hide puestos (no se como pasarle el id a HTTP delete ya que no estamos en la vista del detalle de la pelicula si no en la vista de la lista de peliculas)

--- ASIP1/vue-example-master/src/app/entities/movie/MovieList.vue
+++ ASIP1/vue-example-master/src/app/entities/movie/MovieList.vue
@@ -17,6 +17,19 @@
          :to="{ name: 'MovieDetail', params: { id: movie.idMovie }}">
           {{ movie.name }}
         </router-link>
+    <div class="buttone">
+    <button
+        class="eliminado"
+        v-if="isAdmin"
+        variant="primary"
+        @click="eliminar()">Delete</button>
+
+    <button
+        class="ocultado"
+        v-if="isAdmin"
+        variant="primary"
+        @click="ocultar()">Hide</button>
+    </div>
     </div>
   </LoadingPage>
 </template>
@@ -50,6 +63,26 @@ export default {
        this.error = err.message
      })
     .finally(() => this.loading = false)
+  },
+  methods: {
+    eliminar(){
+        HTTP.delete(`movies/${this.$route.params.id}`)
+        .then(this._successHandler)
+        .catch(this._errorHandler)
+      },
+
+    ocultar(){
+        HTTP.delete(`movies/${this.$route.params.id}`)
+        .then(this._successHandler)
+        .catch(this._errorHandler)
+      },
+    
+    _successHandler(response) {
+      this.$router.replace({ name: 'MovieList'})
+    },
+    _errorHandler(err) {
+      this.error = err.response.data.message
+    }
   }
 }
 </script>
@@ -57,7 +90,56 @@ export default {
 <style scoped lang="scss">
   .namemovie {
     margin-top :10px;
+    float:left;
+    padding:20px;
+    float-left:20px;
+    margin-left :10px;
   }
+  .eliminado {
+    background-color: #f44336; 
+    border: none;
+    color: white;
+    padding: 8px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 12px;
+    border-radius: 8px;
+    -webkit-transition-duration: 0.4s;
+    transition-duration: 0.4s;
+  }
+
+  .eliminado:hover {
+    background-color: #f44336;
+    color: white;
+  
+}
+.ocultado {
+    background-color: #555555; 
+    border: none;
+    color: white;
+    padding: 8px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 12px;
+    border-radius: 8px;
+    -webkit-transition-duration: 0.4s;
+    transition-duration: 0.4s;
+  }
+
+  .ocultado:hover {
+    background-color: #555555;
+    color: white;
+  
+}
+.buttone{
+  margin-top:10px;
+
+}
+
+
+
 
 </style>
 
