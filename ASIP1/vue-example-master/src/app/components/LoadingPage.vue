36efabd798ee855fa142a7811d15735e764d1ed0
Sandra Dopico Cantarero <sandra.dopico1@udc.es>
2018-12-03 12:37:39 +0100

    spinner y imagenes

--- ASIP1/vue-example-master/src/app/components/LoadingPage.vue
+++ ASIP1/vue-example-master/src/app/components/LoadingPage.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
-    <div
-      v-if="loading"
-      class="loading">
-      Loading...
+    <div v-if="loading" class="loading">
+      <spinner 
+        message="Loading..." 
+       size="huge" 
+        line-fg-color="#ff0000">
+     </spinner>
     </div>
     <slot/>
   </div>
 </template>
 
-<script>
 
+<script>
+  import Vue from 'vue'
+  import Spinner from 'vue-simple-spinner'
 export default {
   name: 'LoadingPage',
+  components: {
+    Spinner
+  },
+
   props: {
     loading: {
       type: Boolean,
@@ -28,3 +36,9 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+
+
+
+</style>
